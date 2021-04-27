1f1de0a3a8e9ddcb3b92c3687ffd104b1e8c8cb0
DivinerWJ <2574114945@qq.com>
2021-04-27 10:14:43 +0800

    build: 这是一个测试commitlint和cz的提交

---
+++ src/网络相关复习/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: wangjie59
+ * @Date: 2021-04-27 08:54:38
+ * @LastEditors: wangjie59
+ * @LastEditTime: 2021-04-27 09:01:05
+ * @Description: index
+ * @FilePath: /weixin/Users/wangjie/Documents/study/github/notes/src/网络相关复习/index.js
+ */
+
+const a = 1;
+console.log('a', a);
